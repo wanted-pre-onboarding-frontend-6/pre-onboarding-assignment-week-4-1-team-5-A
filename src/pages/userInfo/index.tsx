ae1d41b6d7efb9323079ba1c92e1e344f3cdb85b
seriparkdev <qkrtpfl9228@gmail.com>
2022-09-24 00:17:54 +0900

    feat: Add user detail page

--- src/pages/userInfo/index.tsx
+++ src/pages/userInfo/index.tsx
@@ -1,8 +1,24 @@
-const UserInfoPage = () => {
+function UserInfoPage({ user }) {
+  const userGenderHandler = (genderNum: number) => {
+    if (genderNum === 1 || genderNum === 3) {
+      return 'male';
+    } else {
+      return 'female';
+    }
+  };
+
   return (
     <div>
-      <h1>userinfo Page</h1>
+      <img src={user.photo} alt={user.name} />
+      <div>{user.name}</div>
+      <div>{user.email}</div>
+      <div>{user.age}</div>
+      <div>{userGenderHandler(user.gender_origin)}</div>
+      <div>{user.phone_number}</div>
+      <div>{user.birth_date}</div>
+      <div>{user.address}</div>
+      <div>{user.detail_address}</div>
     </div>
   );
-};
+}
 export default UserInfoPage;
