306ac455277c2ae7a73966cc6288d083ea5b7a45
seriparkdev <qkrtpfl9228@gmail.com>
2022-09-23 23:23:53 +0900

    feat: Add User list

--- src/pages/userList/index.tsx
+++ src/pages/userList/index.tsx
@@ -1,19 +1,63 @@
-// import ListTable from 'container/ListTable/Table';
+import { userAtom, userListAtom } from 'atom/user/atom';
 import useListUser from 'queries/user/useListUser';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { UserList, UserSetting } from 'types/user';
+import useUserSetting from 'queries/user/useUserSetting';
+import ListTable from 'container/ListTable/Table';
+import accountApi from 'apis/account/accountApi';
 
 const UserListPage = () => {
-  const { data, isLoading, isError } = useListUser({
+  const userLists = useListUser({
+    _page: 1,
+    _limit: 10,
+    _sort: 'desc',
+  });
+  const { data } = useUserSetting({
     _page: 1,
     _limit: 10,
     _sort: 'desc',
   });
 
-  console.log(data, isLoading, isError);
+  const getAccountCount = async (id: number) => {
+    const res = await accountApi.getAccountbyId(`/?user_id=${id}`);
+    const array = res.data;
+    const count = array.length;
+    res &&
+      res.forEach((item: any) => {
+        item.name = id;
+        item.count = count;
+      });
+  };
+
+  console.log(getAccountCount(3));
+
+  const userSetting = data?.data;
+  const [userList, setUserList] = useRecoilState<any>(userListAtom);
+  const [userAll, setUserAll] = useRecoilState<any>(userAtom);
+
+  const mergeArrayObjects = (userList: UserList[], userSetting: UserSetting[]) => {
+    return (
+      userSetting &&
+      userSetting.map((item, i) => {
+        const data = getAccountCount(item.id);
+        if (item.uuid === userList[i]?.uuid ===) {
+          const newArray = Object.assign({}, item, userList[i]);
+          return newArray
+        }
+      })
+    );
+  };
+
+  const userAllData = mergeArrayObjects(userList, userSetting);
+
+  useEffect(() => {
+    setUserList(userLists.data?.data);
+  }, [userLists, setUserAll]);
 
   return (
     <div>
-      <h1>userList Page</h1>
-      {/* <ListTable type="user" /> */}
+      <ListTable type="user" />
     </div>
   );
 };
